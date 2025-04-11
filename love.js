a08ebef0001e96428b8682dc2c5dad322b4dee15
Adarsh Rakesh Srivastav <adsrivastav179@gmail.com>
2025-04-11 21:04:02 +0530

    Add index.html

---
+++ love.js
@@ -0,0 +1,55 @@
+ // Fill page with "sorry bbbuuu"
+ const bg = document.getElementById("bgText");
+ for (let i = 0; i < 300; i++) {
+   const span = document.createElement("span");
+   span.textContent = "sorry bbbuuu";
+   bg.appendChild(span);
+ }
+
+ function showLove(type) {
+   const msg = document.getElementById("loveMsg");
+   msg.style.display = "flex";
+
+   if (type === 'jaan') {
+     msg.innerHTML = `
+       <div class="sticker-card">
+         💖 I loveeee you sooo much jaan! 😘 💖🙌<br><br>
+         Blessed to have you in my life my love 💖my wife 😘😘💖
+       </div>
+     `;
+   } else {
+     msg.innerHTML = `
+       <div class="sticker-card">
+         😔 I love you bchhaaaa❤💖bohttt jyadaaaaa🙌❤<br><br>
+        babbuuuu hu nh aapka😒 maaf kr do na yrrrr 😣
+       </div>
+     `;
+   }
+ } // Fill page with "sorry bbbuuu"
+ const msg = document.getElementById("bgText");
+ for (let i = 0; i < 300; i++) {
+   const span = document.createElement("span");
+   span.textContent = "sorry bbbuuu";
+   bg.appendChild(span);
+ }
+
+ function showLove(type) {
+   const msg = document.getElementById("loveMsg");
+   msg.style.display = "flex";
+
+   if (type === 'jaan') {
+     msg.innerHTML = `
+       <div class="sticker-card">
+         💖 I loveeee you sooo much jaan! 😘 💖🙌<br><br>
+         Blessed to have you in my life my love 💖my wife 😘😘💖
+       </div>
+     `;
+   } else {
+     msg.innerHTML = `
+       <div class="sticker-card">
+         😔 I love you bchhaaaa❤💖bohttt jyadaaaaa🙌❤<br><br>
+        babbuuuu hu nh aapka😒 maaf kr do na yrrrr 😣
+       </div>
+     `;
+   }
+ }
